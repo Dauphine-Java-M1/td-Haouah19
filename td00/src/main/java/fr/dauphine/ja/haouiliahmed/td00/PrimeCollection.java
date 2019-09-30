67a142b1c682cc6daabce03d7abfd13ab74635bb
haouah19 <haouah19@b028-21.crio.dauphine.fr>
2019-09-30 15:05:53 +0200

    Question 4

---
+++ td00/src/main/java/fr/dauphine/ja/haouiliahmed/td00/PrimeCollection.java
@@ -0,0 +1,50 @@
+package fr.dauphine.ja.haouiliahmed.td00;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Hello world!
+ *
+ */
+public class PrimeCollection 
+{
+	private ArrayList<Integer> numbers;
+	private Random random = new Random();
+	public PrimeCollection() 
+	{
+		this.numbers = new ArrayList<>();
+	}
+	
+	public void initRandon(int n,int m)
+	{
+		for(int i = 0 ; i<=n; i++) {
+			int rajout = random.nextInt(m);
+			numbers.add(rajout);
+		}
+	}
+	private boolean isPrime(int p) 
+	{
+		if(p<1) return false;
+		for(int i =2; i<Math.sqrt(p); i++) {
+			if(p%i == 0) return false;
+		}
+		return true;
+	}
+	
+	public void printPrimes() 
+	{
+		for(Integer e : this.numbers) {
+			if(isPrime(e)) {
+				System.out.println(e);
+			}
+		}
+	}
+	
+    public static void main( String[] args )
+    {
+       PrimeCollection primeCollection = new PrimeCollection();
+       primeCollection.initRandon(100, 1000);
+       primeCollection.printPrimes();
+    }
+}
