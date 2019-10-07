ff3b5d77d5e4886976e41a5b2c21825555755e15
haouah19 <haouah19@b028-20.crio.dauphine.fr>
2019-10-07 14:45:54 +0200

    td01

---
+++ shapes/src/main/java/fr/dauphine/ja/haouiliahmed/shapes/Point.java
@@ -0,0 +1,11 @@
+package fr.dauphine.ja.haouiliahmed.shapes;
+
+public class Point {
+	private int x;
+	private int y;
+	
+	public void main (String [] args) {
+		Point p = new Point();
+		System.out.println(p.x +" "+p.y);
+	}
+}
