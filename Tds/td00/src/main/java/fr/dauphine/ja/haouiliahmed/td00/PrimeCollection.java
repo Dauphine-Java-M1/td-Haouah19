59a08d92595cacc60f87ca8f6de563dca5b57aa7
haouah19 <haouah19@b028-20.crio.dauphine.fr>
2019-10-07 14:01:19 +0200

    Ajout de Td00 et Reponses.txt

---
+++ Tds/td00/src/main/java/fr/dauphine/ja/haouiliahmed/td00/PrimeCollection.java
@@ -0,0 +1,57 @@
+package fr.dauphine.ja.haouiliahmed.td00;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Hello world!
+ *
+ */
+public class PrimeCollection 
+{
+	// Rajout commentaire pour test commit
+	
+	private ArrayList<Integer> numbers;
+	private Random random = new Random();
+	public PrimeCollection() 
+	{
+		this.numbers = new ArrayList<>();
+	}
+	
+	public void initRandon(int n,int m)
+	{
+		for(int i = 0 ; i<=n; i++) {
+			int rajout = random.nextInt(m);
+			numbers.add(rajout);
+		}
+	}
+	/**
+	 *  Modification de la methode isPrime() pour pouvoir y accéder.
+	 * @param p
+	 * @return
+	 */
+	public boolean isPrime(int p) 
+	{
+		if(p<1) return false;
+		for(int i =2; i<Math.sqrt(p); i++) {
+			if(p%i == 0) return false;
+		}
+		return true;
+	}
+	
+	public void printPrimes() 
+	{
+		for(Integer e : this.numbers) {
+			if(isPrime(e)) {
+				System.out.println(e);
+			}
+		}
+	}
+	
+    public static void main( String[] args )
+    {
+       PrimeCollection primeCollection = new PrimeCollection();
+       primeCollection.initRandon(100, 2000);
+       primeCollection.printPrimes();
+    }
+}
